264e8dc98487e73a74d36db3daa5e583aa5a0f34
Anya A M <anmoh23@student.sdu.dk>
2025-05-29 15:28:18 +0200

    Spring framework!

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -3,9 +3,13 @@ import dk.sdu.mmmi.cbse.common.services.ISoundService;
 
 module Core {
     requires Common;
-    requires CommonBullet;    
+    requires CommonBullet;
     requires javafx.graphics;
-    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.core;
+    exports dk.sdu.mmmi.cbse.main;
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics,spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
