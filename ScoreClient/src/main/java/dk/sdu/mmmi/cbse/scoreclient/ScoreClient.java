6c0cd04e318c76915885b21310ff439d02500a72
Anya A M <anmoh23@student.sdu.dk>
2025-05-30 22:07:19 +0200

    Adding try-catch in ScoreClient

--- ScoreClient/src/main/java/dk/sdu/mmmi/cbse/scoreclient/ScoreClient.java
+++ ScoreClient/src/main/java/dk/sdu/mmmi/cbse/scoreclient/ScoreClient.java
@@ -1,6 +1,7 @@
 package dk.sdu.mmmi.cbse.scoreclient;
 
 import dk.sdu.mmmi.cbse.common.services.IScoringService;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 public class ScoreClient implements IScoringService {
@@ -9,13 +10,22 @@ public class ScoreClient implements IScoringService {
 
     @Override
     public int getScore() {
-        String response = restTemplate.getForObject(pointsURL, String.class);
-        return Integer.parseInt(response);
+        try {
+            String response = restTemplate.getForObject(pointsURL, String.class);
+            return Integer.parseInt(response);
+        } catch (Exception e) {
+            System.err.println("Error getting the score: " + e.getMessage());
+            return 0;
+        }
     }
 
     @Override
     public void sendScore(int point) {
-        String url = pointsURL + "/" + String.valueOf(point);
-        restTemplate.put(url, null);
+        try {
+            String url = pointsURL + "/" + String.valueOf(point);
+            restTemplate.put(url, null);
+        } catch (Exception e) {
+            System.err.println("Error sending the score: " + e.getMessage());
+        }
     }
 }
